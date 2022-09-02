b5c8b55e62293a11f45e2e2df4f4148c5d719a7a
qindewan <1419497892@qq.com>
2022-09-02 21:13:14 +0800

    master commit1

--- src/main/java/com/example/base/controller/BaseController.java
+++ src/main/java/com/example/base/controller/BaseController.java
@@ -65,6 +65,7 @@ public class BaseController {
         System.out.println("hello git 1");
         System.out.println("hello git 2");
         System.out.println("hello git 3");
+        System.out.println("master test");
         lock.lock();
         try {
             return buy_good();
